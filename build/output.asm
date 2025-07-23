7131a42022c20be9bcd13388fee3a44e6fba08da
Bi1ku <2008owenshi@gmail.com>
2025-07-23 12:07:06 -0400

    fix lexer

--- build/output.asm
+++ build/output.asm
@@ -1,37 +1,8 @@
 .data
-	yo_mama: .float -15.000000
-	suggo_my_eggos: .word -36
-	x: .word 7
-	test: .word 8
-	p: .word 9
-	suiiiiii: .float 2323125.000000
-	type: .asciiz "hello, world!"
-	res: .asciiz "test 123"
-	rando_float: .float 1.120000
 
 .text
 .globl main
 main:
-	li $v0, 2
-	lwc1 $f12, yo_mama
-	syscall
-	li $v0, 4
-	la $a0, type
-	syscall
-	lwc1 $f0, 1.120000
-	jal fart
-	li $v0, 2
-	lwc1 $f12, rando_float
 	syscall
 	li $v0, 10
-	syscall
-solve:
-	li $v0, 7
-	jr $ra
-ligma:
-	l.s $f1, suiiiiii
-	jr $ra
-fart:
-	li $v0, 4
-	la $a0, res
 	syscall
